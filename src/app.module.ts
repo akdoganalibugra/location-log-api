060eb32038ff24654bcc83ec50a72d8b9bf4e887
Ali Bugra <akdoganalibugra@gmail.com>
2025-06-25 00:15:57 +0300

    feat: add LocationsModule to AppModule imports for enhanced location processing

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AreasModule } from './areas/areas.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [DatabaseModule, AreasModule],
+  imports: [DatabaseModule, AreasModule, LocationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
